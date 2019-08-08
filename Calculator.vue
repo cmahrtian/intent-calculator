b38e0ebefc3fc84c4ed29907e7550a0e1a3aaf48
Chris Mahr <mahrtian@gmail.com>
2019-08-07 22:08:50 -0400

    1. create buttonRows array of arrays 2. iterate through each array in buttonRows, create <li> for each - in each array, create button wrapped in span with button content 3. remove bulleted formatting from <ul> tag

--- Calculator.vue
+++ Calculator.vue
@@ -1,15 +1,36 @@
 <script>
 export default {
-  
+  data () {
+    return {
+      buttonRows: [
+        ['AC', '+/-', '%', '÷'],
+        [7, 8, 9, 'X'],
+        [4, 5, 6, '-'],
+        [1, 2, 3, '+'],
+        [0, '.', '=']
+      ]
+    }
+  }
 }
 </script>
 
 <template>
-  <div>
-    <h1>Calculator goes here!!!</h1>
+  <div class="calculator">
+    <h1>Calculations go here</h1>
+    <ul>
+      <li v-for="(row, index) in buttonRows"
+        :key="index">
+        <span v-for="(button, index) in row"
+          :key="index">
+          <button>{{ button }}</button>
+        </span>
+      </li>
+    </ul>
   </div>
 </template>
 
-<style lang="scss" scoped>
-
+<style lang="css" scoped>
+  .calculator ul {
+    list-style-type: none;
+  }
 </style>
